1901610e191c26d820be1dfdb0ac24ed116a76e7
João Pedro <jp663687@gmail.com>
2023-06-25 12:02:04 -0300

    Att Ordem

---
+++ HardWareTech.DATA/Repositories/RepositoryCliente.cs
@@ -0,0 +1,18 @@
+﻿using HardWareTech.DATA.Interfaces;
+using HardWareTech.DATA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HardWareTech.DATA.Repositories
+{
+    public class RepositoryCliente : RepositoryBase<Cliente>, IRepositoryCliente
+    {
+        public RepositoryCliente(bool SaveChanges = true) : base(SaveChanges)
+        {
+
+        }
+    }
+}
---
+++ HardWareTech.WEB/HardWareTech.WEB/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using FastReport.Export.PdfSimple;
+using HardWareTech.DATA.Services;
+using HardWareTech.WEB.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HardWareTech.WEB.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly VwProdutoClienteManutencaoService ovwProdutoClienteManutencaoService;
+        private readonly ProdutoService oprodutoService;
+
+        public HomeController()
+        {
+            ovwProdutoClienteManutencaoService = new VwProdutoClienteManutencaoService();
+            oprodutoService = new ProdutoService();
+        }
+
+        public IActionResult Index()
+        {
+            var totalProdutos = ovwProdutoClienteManutencaoService.oRepositoryProduto.SelecionarTodos().Count;
+            var totalClientes = ovwProdutoClienteManutencaoService.oRepositoryCliente.SelecionarTodos().Count;
+            var totalManutencoes = ovwProdutoClienteManutencaoService.oRepositoryProdutoClienteManutencao.SelecionarTodos().Count;
+            var totalCategorias = ovwProdutoClienteManutencaoService.oRepositoryCategoria.SelecionarTodos().Count;
+            ViewBag.TotalClientes = totalClientes;
+            ViewBag.TotalProdutos = totalProdutos;
+            ViewBag.TotalManutencoes = totalManutencoes;
+            ViewBag.TotalCategorias = totalCategorias;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
